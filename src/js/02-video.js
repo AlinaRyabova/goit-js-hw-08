c9d5a124e0ab06b9014b1d0bee7dc87d848dd165
AlinaRyabova <115992255+AlinaRyabova@users.noreply.github.com>
2023-03-31 16:46:07 +0300

    add

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,30 +1,20 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const iframe = document.querySelector('iframe');
-const player = new Player(iframe);
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
 
-const onPlay = function (data) {
-    localStorage.setItem('videoplayer-current-time', data.seconds);
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe, {
+  loop: true,
+  fullscreen: true,
+  quality: '1080p',
+});
+
+const getCurrentTime = function (currentTime) {
+  const seconds = currentTime.seconds;
+  localStorage.setItem(CURRENT_TIME_KEY, JSON.stringify(seconds));
 };
 
-player.on('timeupdate', throttle(onPlay, 1000));
+player.on('timeupdate', throttle(getCurrentTime, 1000));
 
-const currentTime = Number(localStorage.getItem('videoplayer-current-time'));
-
-player.setCurrentTime(currentTime).then(function (seconds) {
-    
-}).catch(function (error) {
-    switch (error.name) {
-        case 'RangeError':
-            break;
-        default:
-            break;
-    }
-});
-
-player.setColor('#45a247').then(function (color) {
-   
-}).catch(function (error) {
-    
-});
+player.setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_TIME_KEY)) || 0);
